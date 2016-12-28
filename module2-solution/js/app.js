11e946be70bf5df76a5d906e7ca175b91e497ce8
artespoon <artespoon@gmail.com>
2016-12-28 20:52:48 +0100

    Initial solution for module 2

---
+++ module2-solution/js/app.js
@@ -0,0 +1,68 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+	var toBuy = this;
+  	
+  	toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  	toBuy.bought = function (itemIndex) {
+  		ShoppingListCheckOffService.bought(itemIndex);
+  	}
+};
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  	var alreadyBought = this;
+  	
+  	alreadyBought.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+};
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var toBuyItems = [
+	{ 
+	  name: "cookies",
+      quantity: 10
+    },
+    { 
+	  name: "bubble gum",
+      quantity: 2
+    },
+    { 
+	  name: "coca cola",
+      quantity: 3
+    },
+    { 
+	  name: "bread",
+      quantity: 1
+    },
+    { 
+	  name: "orange juce",
+      quantity: 6
+    },
+  ];
+  var alreadyBoughtItems = [];
+
+  service.bought = function (itemIndex) {
+    var boughtItem = toBuyItems[itemIndex]
+    toBuyItems.splice(itemIndex, 1);
+    alreadyBoughtItems.push(boughtItem);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getAlreadyBoughtItems = function () {
+    return alreadyBoughtItems;
+  };
+}
+})();
